f5406675d1886485ddbc087e900795e5c3dfcdb7
Artem-Koren <137041733+Artem-Koren@users.noreply.github.com>
2023-08-08 18:45:37 +1000

    Add files via upload

---
+++ advertisment/app_advertisment/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Advertisement
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    return render(request, 'advertisement_post.html')
+
+def debug(request):
+    for i in range(100):
+        ad = Advertisement(
+            title=f"Тестовое объявление {i}",
+            text="Текст объявления",
+            author="admin"
+        )
+        ad.save()
+        ob = Advertisement.objects.all()
+        print(ob)
+    return HttpResponse("")
